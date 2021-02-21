e104cffcd69133ed0a548db77eb7c180b922ef00
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2021-02-21 17:53:20 +0800

    repetitions prob done

--- repetitions.cpp
+++ repetitions.cpp
@@ -1,10 +1,29 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using std::cin;
 using std::cout;
 using std::endl;
-
+using std::string;
+typedef long long ll;
 int main()
 {
+    ll count, max = 1;
+    char c;
+    string dna;
+    cin >> dna;
+    ll length = dna.length();
+    for (ll i = 0; i < length-1; )
+    {
+        c = dna[i];
+        count = 1;
+        for (ll j = i + 1; j < length; j++)
+        {
+            if(dna[j] == c) count++;
+            else break;
+        }
+        if(count > max) max = count;
+        i = i+count; // skip a section of similar characters
+    }
 
+    cout << max << endl;
     return 0;
 }
